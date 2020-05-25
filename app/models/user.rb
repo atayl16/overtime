9a99c0b8a2f6c738e3aed48a97c910cad1ba220d
Alisha Taylor <atayl16@yahoo.com>
2020-05-25 20:26:13 +0100

    refactor posts, specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    first_name.upcase + " " + last_name.upcase
+  end
+
 end
